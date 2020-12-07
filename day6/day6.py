f48aebecc80aff7a27a7b968e7ceb3322ac0dab5
bbond2980 <bbond2980@gmail.com>
2020-12-07 10:11:17 -0500

    day 6 part 2 add, one day late

--- day6/day6.py
+++ day6/day6.py
@@ -1,4 +1,4 @@
-def customsdec():
+def customsdecP1():
     with open("/Users/benjaminbond/Desktop/AoC/day6/day6input.txt", 'r') as f:
 
         data = f.read()
@@ -24,7 +24,36 @@ def customsdec():
         sum = 0
         for item in newnewdecl:
             sum += len(item)
-        print(sum)
+        return sum
 
+def customsdecP2():
+    with open("/Users/benjaminbond/Desktop/AoC/day6/day6input.txt", 'r') as f:
 
-customsdec()
+        data = f.read()
+        #print(data)
+        decl = []
+        for block in data.split('\n\n'):
+         ###   print(block)
+            decl.append(block.strip('\n'))
+        splitgroups = list()
+        for group in decl:
+            splitgroups.append(group.split('\n'))
+        #print(splitgroups)
+        totallist = list()
+        
+        for item in splitgroups:
+            totalsum = 0
+            first = item[0]
+            for letter in first:
+                present = True
+                for other in item[1:]:
+                    if letter not in other:
+                        present = False
+                if present == True:
+                    totalsum += 1
+            totallist.append(totalsum)
+        print(sum(totallist))
+                        
+            
+                
+customsdecP2()
